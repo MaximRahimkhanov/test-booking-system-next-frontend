9ca4bf44d57094cd66bac8785136e0b41ec2cd09
MaximRahimkhanov <zloyigrog@gmail.com>
2026-01-16 12:46:25 +0200

    fix error vercel

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from 'next/server';
 import { getAllUsers } from '@/lib/api';
+import { isAxiosError } from 'axios';
+import { logErrorResponse } from '../utils/utils';
 
 export async function GET() {
   try {
     const users = await getAllUsers();
     return NextResponse.json(users);
   } catch (error) {
-    return NextResponse.json({ message: 'Помилка сервера' }, { status: 500 });
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+
+      return NextResponse.json({ message: 'Помилка сервера' }, { status: 500 });
+    }
   }
 }
